20c34a38dd02de4d0c668c51dcedbb66753e15b2
SatoshiLiang <chihchengliang@gmail.com>
2014-10-08 16:41:30 +0800

    add submission log feature

--- handlers.py
+++ handlers.py
@@ -7,6 +7,20 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 from webapp2_extras.appengine.users import *
 from independent_unit import *
+from datetime import datetime
+
+# don't modify the existing element in this list
+
+problem_sets = [
+    'helloworld',
+    'rot13',
+    'signup',
+    'blog',
+    'registration',
+    'login',
+    'logout',
+    'json',
+    ]
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -52,14 +66,23 @@ class MiniJudge(BaseHandler):
 
     @login_required
     def get(self):
-        self.render('minijudge.html', nickname=self.nickname())
+        self.render('minijudge.html', nickname=self.nickname(),
+                    problem_sets=problem_sets)
 
     def post(self):
+
+        pb_set = self.request.get('pb_set')
 
         (url, success, msg) = ping_url(url=self.request.get('inputURL'))
         print (url, success, msg)
 
-        self.updateUser(url, success, msg)
+        if pb_set in problem_sets:
+            if pb_set == problem_sets[0]:
+                self.updateUser(url, success, msg)
+            else:
+                self.updateSubmission(pb_set=pb_set, url=url,
+                        is_success=success, msg=msg)
+
         self.render('minijudge.html', msg=msg, nickname=self.nickname())
 
         if success:
@@ -91,14 +114,64 @@ class MiniJudge(BaseHandler):
                         site=url, is_success=is_success, msg=msg)
                 new_u.put()
 
+    def updateSubmission(
+        self,
+        pb_set='',
+        url='',
+        is_success=False,
+        msg='',
+        ):
+
+        user = users.get_current_user()
+        if user:
+            uid = user.user_id()
+            q = ndb.gql('SELECT * FROM User WHERE uid = :1', uid)
+            u = q.get()
+            if u:
+                q2 = \
+                    ndb.gql('SELECT * FROM Submissions WHERE uid = :1 and pb_set = :2'
+                            , uid, pb_set)
+                s = q2.get()
+                if s:
+                    s.url = url
+                    s.is_success = is_success
+                    s.msg = msg
+                    s.updated = datetime.utcnow()
+                    s.put()
+                else:
+                    submission = Submissions(
+                        uid=uid,
+                        name=u.name,
+                        pb_set=pb_set,
+                        url=url,
+                        is_success=is_success,
+                        msg=msg,
+                        updated=datetime.utcnow(),
+                        )
+                    submission.put()
+            else:
+
+                # in case the user not exist, use current url to register one.
+
+                new_u = User.register(uid=uid, name=user.nickname(),
+                        site=url, is_success=is_success, msg=msg)
+                new_u.put()
+
 
 class User(ndb.Model):
 
     uid = ndb.StringProperty(required=True)
     name = ndb.StringProperty()
+
+    # Root site
+
     site = ndb.StringProperty()
     is_success = ndb.BooleanProperty()
+
+    # the message that minijudge said
+
     msg = ndb.StringProperty()
+
     created = ndb.DateTimeProperty(auto_now_add=True)
 
     @classmethod
@@ -129,28 +202,27 @@ class User(ndb.Model):
         return cls.query().order(-cls.created)
 
 
-#
-#    @classmethod
-#    def login(cls, name, pw):
-#        u = cls.by_name(name)
-#        if u and valid_pw(name, pw, u.pw_hash):
-#            return u
+class Submissions(ndb.Model):
 
-#        u = User.by_name(self.username)
-#        if u:
-#            msg = 'The user already exists.'
-#            self.write('signup.html', user_error=msg)
-#        else:
-#            u = User.register(self.username, self.password, self.email)
-#        u.put()
-#
-#        self.login(u)
-#        self.redirect('/welcome')
+    uid = ndb.StringProperty(required=True)
+    name = ndb.StringProperty()
+    pb_set = ndb.StringProperty(required=True)
+    url = ndb.StringProperty()
+    is_success = ndb.BooleanProperty()
+    msg = ndb.StringProperty()
+    updated = ndb.DateTimeProperty(auto_now_add=True)
+
+    @classmethod
+    def query_all(cls):
+        return cls.query().order(-cls.updated)
+
 
 class DashBoard(BaseHandler):
 
     def get(self):
         users = User.query_all()
-        self.render('dashboard.html', users=users)
+        submissions = Submissions.query_all()
+        self.render('dashboard.html', users=users,
+                    submissions=submissions)
 
 
